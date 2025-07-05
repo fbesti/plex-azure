c6f2334ebb22853ab6981da562c2d51750f0da6f
fbesti <freyrbesti@gmail.com>
2025-07-05 12:19:57 +0000

    test

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,4 +1,10 @@
 terraform {
+    backend "azurerm" {
+      resource_group_name  = "rg-terraform-states-prod"
+      storage_account_name = "stofustatesprdfbesti8106"
+      container_name       = "tofu-state"
+      key                  = "terraform.tfstate"
+    }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
