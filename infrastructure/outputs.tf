75f7863899a321dcda1fffc3ba09e131c85b52c7
fbesti <freyrbesti@gmail.com>
2025-07-05 17:56:46 +0000

    fix open tofu code

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,4 +1,4 @@
 output "resource_group_name" {
   description = "The name of the created resource group."
-  value       = azurerm_resource_group.example.name
+  value       = azurerm_resource_group.plex_cert.name
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,7 +7,7 @@ variable "location" {
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-  default     = "rg-example"
+  default     = "rg-plex-cert"
 }
 
 variable "remote_state_subscription_id" {
