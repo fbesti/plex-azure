bae8eacbbce8baf58b10486fde48c1ab0d597ac8
Freyr Finnbogason <freyrbesti@gmail.com>
2025-07-07 14:53:59 +0000

    Adding test rg

--- infrastructure/azure/main.tf
+++ infrastructure/azure/main.tf
@@ -1,5 +1,10 @@
-
 resource "azurerm_resource_group" "plex_cert" {
   name     = var.resource_group_name
+  location = var.location
+}
+
+
+resource "azurerm_resource_group" "plex_test" {
+  name     = var.resource_group_name_test
   location = var.location
 }
