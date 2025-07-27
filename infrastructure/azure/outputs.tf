7f12141f4e405e4272534833a864250b92e6d52e
Freyr Finnbogason <freyrbesti@gmail.com>
2025-07-27 10:58:51 +0000

    me fix

--- infrastructure/azure/outputs.tf
+++ infrastructure/azure/outputs.tf
@@ -2,8 +2,3 @@ output "resource_group_name" {
   description = "The name of the created resource group."
   value       = azurerm_resource_group.plex_cert.name
 }
-
-output "app_gateway_zones" {
-  description = "The availability zones of the application gateway."
-  value       = azurerm_application_gateway.app_gw_test.zones
-}
